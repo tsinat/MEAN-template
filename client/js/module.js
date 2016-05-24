b25bbc7d1c7aa7bdef8b34bc5c210420a26b968f
Tsinat Zeree <cheru87@gmail.com>
2016-05-23 22:00:24 -0700

    initial commit

---
+++ client/js/module.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var app = angular.module("myApp", []);
+
+app.controller("mainCtrl", function($scope, $http) {
+    console.log("Main Controller");
+
+});
+
+app.controller('visionCtrl', function($scope, $http) {
+
+});
